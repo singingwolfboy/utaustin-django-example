1ed981e2ed4f6cf73e14a3f5693ee48f08eabe23
David Baumgold <david@davidbaumgold.com>
2017-01-03 15:55:01 -0600

    class-based views

--- proj/tweeter/urls.py
+++ proj/tweeter/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from tweeter import views
 
 urlpatterns = [
-    url(r'users/(?P<username>\w+)', views.view_user, name='view_user'),
+    url(
+        r'users/(?P<username>\w+)',
+        views.UserProfileView.as_view(),
+        name='user_profile',
+    ),
     url(r'tweet', views.new_tweet, name='new_tweet'),
     url(r'', views.home_page, name='home_page'),
 ]
--- proj/tweeter/views.py
+++ proj/tweeter/views.py
@@ -2,9 +2,11 @@ from __future__ import print_function
 
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect, HttpResponseBadRequest
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.views import View
+from django.urls import reverse
 from tweeter.models import Tweet, Profile
 from tweeter.forms import TweetForm, UserProfileForm
 
@@ -20,41 +22,52 @@ def home_page(request):
     return render(request, "home_page.html", context)
 
 
-@require_http_methods(["GET", "POST"])
-def view_user(request, username):
-    try:
-        user = User.objects.filter(username=username).get()
-    except User.DoesNotExist:
-        raise Http404("User {username} not found".format(username=username))
+class UserProfileView(View):
 
-    if user == request.user:
-        if request.method == 'POST':
-            profile_form = UserProfileForm(request.POST, instance=user.profile)
-            if profile_form.is_valid():
-                profile_form.save()
-                messages.add_message(
-                    request,
-                    messages.SUCCESS,
-                    'Your profile has been updated.',
-                )
-        else:
+    def get_user(self, username):
+        try:
+            user = User.objects.filter(username=username).get()
+        except User.DoesNotExist:
+            raise Http404("User {username} not found".format(username=username))
+        return user
+
+    def get(self, request, username):
+        user = self.get_user(username)
+
+        if user == request.user:
             profile_form = UserProfileForm(instance=user.profile)
-        tweet_form = TweetForm()
-    else:
-        profile_form = None
-        tweet_form = None
+            tweet_form = TweetForm()
+        else:
+            profile_form = None
+            tweet_form = None
 
-    user_tweets = (
-        Tweet.objects.filter(creator=user)
-        .order_by('-created_at').all()
-    )
-    context = {
-        "viewed_user": user,
-        "tweets": user_tweets,
-        "profile_form": profile_form,
-        "tweet_form": tweet_form,
-    }
-    return render(request, "view_user.html", context)
+        user_tweets = (
+            Tweet.objects.filter(creator=user)
+            .order_by('-created_at').all()
+        )
+        context = {
+            "viewed_user": user,
+            "tweets": user_tweets,
+            "profile_form": profile_form,
+            "tweet_form": tweet_form,
+        }
+        return render(request, "user_profile.html", context)
+
+    def post(self, request, username):
+        user = self.get_user(username)
+        if user != request.user:
+            return HttpResponseBadRequest("You can't modify another user's profile.")
+
+        profile_form = UserProfileForm(request.POST, instance=user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Your profile has been updated.',
+            )
+
+        return HttpResponseRedirect(reverse('user_profile'))
 
 
 @require_http_methods(["POST"])
