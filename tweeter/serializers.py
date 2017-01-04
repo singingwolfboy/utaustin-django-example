291bfc399b80b37dc6eec102da5a3760c365a59f
David Baumgold <david@davidbaumgold.com>
2017-01-04 14:10:20 -0600

    More serializers

--- tweeter/serializers.py
+++ tweeter/serializers.py
@@ -1,10 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from tweeter.models import Tweet
 
 
-class TweetSerializer(serializers.Serializer):
-    content = serializers.CharField()
-    creator = serializers.SerializerMethodField()
-    created_at = serializers.DateTimeField()
+class UserSerializer(serializers.ModelSerializer):
+    bio = serializers.SerializerMethodField()
 
-    def get_creator(self, obj):
-        return obj.creator.username
+    def get_bio(self, obj):
+        return obj.profile.bio
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "bio")
+
+
+class TweetSerializer(serializers.ModelSerializer):
+    creator = UserSerializer()
+
+    class Meta:
+        model = Tweet
+        fields = ('content', 'creator', 'created_at')
