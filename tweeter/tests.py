3b3b4f4e1add9107ee009963ac5ef2b4dc010ba2
David Baumgold <david@davidbaumgold.com>
2017-01-04 08:49:02 -0600

    Getting started with pytest

--- tweeter/tests.py
+++ tweeter/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import Client
+import pytest
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_home_page():
+    c = Client()
+    response = c.get("/")
+    assert response.status_code == 200
+    assert "Tweeter" in response.content
+    # pytest.set_trace()
