2600a166650daa9f84a66cef63131c0a0a0b48b7
David Baumgold <david@davidbaumgold.com>
2017-01-04 09:04:24 -0600

    another test

--- tweeter/tests.py
+++ tweeter/tests.py
@@ -1,4 +1,5 @@
 from django.test import Client
+from django.contrib.auth.models import User
 import pytest
 
 @pytest.mark.django_db
@@ -7,4 +8,17 @@ def test_home_page():
     response = c.get("/")
     assert response.status_code == 200
     assert "Tweeter" in response.content
-    # pytest.set_trace()
+    assert "new user" in response.content
+
+
+@pytest.mark.django_db
+def test_home_page_logged_in():
+    jane = User(username="jane")
+    jane.set_password("testing")
+    jane.save()
+
+    c = Client()
+    c.login(username="jane", password="testing")
+    response = c.get("/")
+    assert response.status_code == 200
+    assert "jane" in response.content
